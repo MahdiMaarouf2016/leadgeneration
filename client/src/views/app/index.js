dba880674f09e0dedf42e2ad91928f864aad638b
Med Mahdi Maarouf <medmahdi.maarouf@gmail.com>
2021-07-07 21:03:11 +0100

    first commit

---
+++ client/src/views/app/index.js
@@ -0,0 +1,93 @@
+import React, { Component, Suspense } from 'react';
+import { Route, withRouter, Switch, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import AppLayout from '../../layout/AppLayout';
+
+const Gogo = React.lazy(() =>
+  import(/* webpackChunkName: "viwes-gogo" */ './gogo')
+);
+const Conv = React.lazy(() =>
+  import(/* webpackChunkName: "viwes-gogo" */ './conversations')
+);
+const SecondMenu = React.lazy(() =>
+  import(/* webpackChunkName: "viwes-second-menu" */ './second-menu')
+);
+const StatsPage = React.lazy(() =>
+  import(/* webpackChunkName: "viwes-stats-page" */ './blank-page')
+);
+const Profile = React.lazy(() =>
+  import(/* webpackChunkName: "viwes-profile" */ './profile')
+);
+const Notes = React.lazy(() =>
+  import(/* webpackChunkName: "viwes-profile" */ './notes')
+);
+const Search = React.lazy(() =>
+  import(/* webpackChunkName: "viwes-profile" */ './search')
+);
+const Admin = React.lazy(() =>
+  import(/* webpackChunkName: "pages" */ './admin')
+);
+
+
+class App extends Component {
+  render() {
+    const { match } = this.props;
+
+    return (
+      <AppLayout>
+        <div className="dashboard-wrapper">
+          <Suspense fallback={<div className="loading" />}>
+            <Switch>
+              <Redirect exact from={`${match.url}/`} to={`${match.url}/gogo`} />
+              <Route
+                path={`${match.url}/gogo`}
+                render={props => <Gogo {...props} />}
+              />
+                 <Route
+                path={`${match.url}/conversations`}
+                render={props => <Conv {...props} />}
+              />
+              <Route
+                path={`${match.url}/second-menu`}
+                render={props => <SecondMenu {...props} />}
+              />
+              <Route
+                path={`${match.url}/admin`}
+                render={props => <Admin {...props} />}
+              />
+              <Route
+                path={`${match.url}/stats-page`}
+                render={props => <StatsPage {...props} />}
+              />
+              <Route
+                path={`${match.url}/profile`}
+                render={props => <Profile {...props} />}
+              />
+              <Route
+                path={`${match.url}/notes`}
+                render={props => <Notes {...props} />}
+              />
+              <Route
+                path={`/app/pages/search`}
+                render={props => <Search {...props} />}
+              />
+              <Redirect to="/error" />
+            </Switch>
+          </Suspense>
+        </div>
+      </AppLayout>
+    );
+  }
+}
+const mapStateToProps = ({ menu }) => {
+  const { containerClassnames } = menu;
+  return { containerClassnames };
+};
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    {}
+  )(App)
+);
---
+++ config/keys.js
@@ -0,0 +1,20 @@
+module.exports = {
+  mongoURI: "mongodb://localhost:27017/crmpfe",
+  secretOrKey: "changeme", 
+  SENDGRID_API_KEY:"apikey", 
+  SMTP_HOST: "localhost", 
+  SMTP_PORT: 25,
+  SMTP_SSL: false, // upgrade later with STARTTLS
+  SMTP_USER:"anonymous", 
+  SMTP_PASS: "anonymous", 
+  APP_URI: "localhost:5000", 
+  FACEBOOK_APP_ID: "3003601446355380", 
+  FACEBOOK_APP_SECRET: "07adcdb65293f560622fc00f67cfa277", 
+  FACEBOOK_CALLBACK_URL: "http://localhost:5000/auth/facebook/callback", 
+  TWITTER_CONSUMER_KEY: 
+  "IbD2taKLYsj8XP96Cec91vb3y", 
+  TWITTER_CONSUMER_SECRET: 
+  "ivRTtldmtZufHa0LjZGajY6nwRpOzAaRXah0ywl0QV8HXjWptY", 
+  TWITTER_CALLBACK_URL: "http://localhost:5000/auth/twitter/callback",
+  TEST_ACCESS:"EAADoZBAD37iEBACwqW2e1jMJmY4mYyU5ZAmsfezNGiFEnpgXVMyTLyuZCy2MSrfdh2ssjA1IZCjy17H5aH8SF8z3ex407MhZA39SvzqeBw9y6rfOn0xCQb7oFxaksACZCrRo6x1GkX7PA59f5uNR5mqrR4S2KCKWPSq1kpWYag3IXEgV4BYDl0"
+};
